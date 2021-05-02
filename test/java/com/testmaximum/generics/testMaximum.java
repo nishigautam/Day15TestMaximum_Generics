d6cfc5b06dcae07b95b879d8861e59db7e205718
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-02 15:34:59 +0530

    testcases added

--- test/java/com/testmaximum/generics/testMaximum.java
+++ test/java/com/testmaximum/generics/testMaximum.java
@@ -23,4 +23,28 @@ public class testMaximum {
         Integer max = t.findMax(9,9,9);
         Assert.assertSame(1, max);
     }
+
+    /**
+     * UC1.1, UC1.2 and UC1.3
+     */
+    @Test
+    public void MaxValueAtFirstPosition() {
+        MaximumValue t = new MaximumValue();
+        Integer max = t.findMax(9,3,2);
+        Assert.assertSame(9, max);
+    }
+
+    @Test
+    public void MaxValueAtSecondPosition() {
+        MaximumValue t = new MaximumValue();
+        Integer max = t.findMax(3,7,2);
+        Assert.assertSame(7, max);
+    }
+
+    @Test
+    public void MaxValueAtThirdPosition() {
+        MaximumValue t = new MaximumValue();
+        Integer max = t.findMax(2,3,8);
+        Assert.assertSame(8, max);
+    }
 }
