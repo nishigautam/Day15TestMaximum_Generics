a2f5916e0688114f09e78d96b2eb23f3f6e6317c
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-03 00:09:06 +0530

    Added:Create generics Class to take in 3 Variables of Generic Type

--- main/java/com/testmaximum/generics/MaximumValue.java
+++ main/java/com/testmaximum/generics/MaximumValue.java
@@ -1,15 +1,30 @@
 package com.testmaximum.generics;
 
-public class MaximumValue {
+import java.util.*;
+
+public class MaximumValue<T extends Comparable<T>> {
+    T num1;
+    T num2;
+    T num3;
+
+    public MaximumValue(T num1, T num2, T num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+
+    public MaximumValue() {
+    }
+
     /**
      * UC 1: method for finding maximum integer value
      */
     public static Integer findMax(Integer num1, Integer num2, Integer num3) {
         Integer max = num1;
-        if(num2.compareTo(max) > 0) {
+        if (num2.compareTo(max) > 0) {
             max = num2;
         }
-        if(num3.compareTo(max) > 0) {
+        if (num3.compareTo(max) > 0) {
             max = num3;
         }
         return max;
@@ -19,11 +34,11 @@ public class MaximumValue {
      * UC 2:  method for finding maximum float value
      */
     public static Float findMax(Float num1, Float num2, Float num3) {
-        float max = num1;
-        if(num2.compareTo(max) > 0) {
+        Float max = num1;
+        if (num2.compareTo(max) > 0) {
             max = num2;
         }
-        if(num3.compareTo(max) > 0) {
+        if (num3.compareTo(max) > 0) {
             max = num3;
         }
         return max;
@@ -34,10 +49,10 @@ public class MaximumValue {
      */
     public static String findMax(String str1, String str2, String str3) {
         String max = str1;
-        if(str2.compareTo(max) > 0) {
+        if (str2.compareTo(max) > 0) {
             max = str2;
         }
-        if(str3.compareTo(max) > 0) {
+        if (str3.compareTo(max) > 0) {
             max = str3;
         }
         return max;
@@ -48,21 +63,29 @@ public class MaximumValue {
      */
     public static <T extends Comparable<T>> T findAnyMax(T num1, T num2, T num3) {
         T max = num1;
-        if(num2.compareTo(max) > 0)
+        if (num2.compareTo(max) > 0)
             max = num2;
-        if(num3.compareTo(max) > 0)
+        if (num3.compareTo(max) > 0)
             max = num3;
         return max;
     }
 
+    public T findAnyMax() {
+        return MaximumValue.findAnyMax(num1, num2, num3);
+    }
+
     /**
      * main function
+     *
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Maximum Value of Integer given is :" + findMax(6,3,9));
-        System.out.println("Maximum value of Float numbers given is :" + findMax(1.0f,5.0f,3.0f));
-        System.out.println("Maximum value of String given is :" + findMax("Boy","Girl","Animal"));
-        System.out.println("Maximum value of String given is :" + findAnyMax("Boy","Girl","Animal"));
+        System.out.println("Maximum Value of Integer given is :" + findMax(6, 3, 9));
+        System.out.println("Maximum value of Float numbers given is :" + findMax(1.0f, 5.0f, 3.0f));
+        System.out.println("Maximum value of String given is :" + findMax("Boy", "Girl", "Animal"));
+        System.out.println("Maximum value of String given is :" + findAnyMax("Boy", "Girl", "Animal"));
+        System.out.println("Maximum Integer by creating Generic class :" + new MaximumValue<Integer>(3, 6, 2).findAnyMax());
     }
 }
+
+
--- test/java/com/testmaximum/generics/testMaximum.java
+++ test/java/com/testmaximum/generics/testMaximum.java
@@ -5,49 +5,37 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class testMaximum {
-    /**
-     * testcase when all integer has different value
-     */
-    @Test
-    public void testCase1() {
-        MaximumValue t = new MaximumValue();
-        Integer max = t.findMax(1,2,3);
-        Assert.assertSame(3, max);
-    }
-
-    /**
-     * testcase when all elements have same value
-     */
-    @Test
-    public void testCase2() {
-        MaximumValue t = new MaximumValue();
-        Integer max = t.findMax(9,9,9);
-        Assert.assertSame(1, max);
-    }
 
     /**
      * UC1.1, UC1.2 and UC1.3
      */
     @Test
     public void MaxValueAtFirstPosition() {
-        MaximumValue t = new MaximumValue();
-        Integer max = t.findMax(9,3,2);
+        Integer max = MaximumValue.findMax(9,3,2);
         Assert.assertSame(9, max);
     }
 
     @Test
     public void MaxValueAtSecondPosition() {
-        MaximumValue t = new MaximumValue();
-        Integer max = t.findMax(3,7,2);
+        Integer max = MaximumValue.findMax(3,7,2);
         Assert.assertSame(7, max);
     }
 
     @Test
     public void MaxValueAtThirdPosition() {
-        MaximumValue t = new MaximumValue();
-        Integer max = t.findMax(2,3,8);
+        Integer max = MaximumValue.findMax(2,3,8);
         Assert.assertSame(8, max);
     }
+
+    /**
+     * testcase when all elements have same value
+     */
+    @Test
+    public void CheckMaxWhenAllElementAreSame() {
+        Integer max = MaximumValue.findMax(9,9,9);
+        Assert.assertSame(9, max);
+    }
+
 
     /**
      * UC2..testcases
@@ -108,5 +96,24 @@ public class testMaximum {
     public void ReturnMaxInteger() {
         Integer max = MaximumValue.findAnyMax(1, 2, 3);
         Assert.assertThat(max, CoreMatchers.equalTo(3));
+    }
+
+    /**
+     * testcases for generics class
+     */
+    @Test
+    public void anyTypeReturnMaxInteger() {
+        Integer max = new MaximumValue<Integer>(1,2,3).findAnyMax();
+        Assert.assertThat(max, CoreMatchers.equalTo(3));
+    }
+    @Test
+    public void anyTypeReturnMaxFloat() {
+        Float max = new MaximumValue<Float>(1.01f, 2.02f, 3.03f).findAnyMax();
+        Assert.assertThat(max, CoreMatchers.equalTo(3.03f));
+    }
+    @Test
+    public void anyTypeReturnMaxString() {
+        String max = new MaximumValue<String>("Boy", "Girl", "Animal").findAnyMax();
+        Assert.assertThat(max, CoreMatchers.equalTo("Girl"));
     }
 }
